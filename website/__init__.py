2a8fc231138eb901252218e5f72bf0fa6f15095c
cougarhighland <xuan_dung.tran0001@stud.hkr.se>
2021-04-27 14:04:20 +0200

    update file from master branch

---
+++ website/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_web():
+    web = Flask(__name__)
+    web.config['SECRET_KEY'] = 'whhhhhaaatteeverr'
+    
+    from website.auth import auth
+
+    web.register_blueprint(auth, url_prefix='/')
+
+    return web
--- website/main.py
+++ website/main.py
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 #
 
 import auth
 
 if __name__ == "__main__":
     auth.Auth.login(auth)
+=======
+
+from website import create_web
+
+web = create_web()
+
+if __name__ == "__main__":
+    web.run(debug=True)
+>>>>>>> main

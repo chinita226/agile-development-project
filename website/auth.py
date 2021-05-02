65938efa3f44cf4c1e76797c962f430d447e4519
Tian Yao <yaotian226@gmail.com>
2021-05-02 19:39:51 +0200

    separate views for restaurant and npo after each logs in

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -13,12 +14,16 @@ def login():
     if request.method == 'POST':
         userName = request.form.get('username')
         password = request.form.get('password')
+        user_type = request.form.get('org_type')
 
         user = User.query.filter_by(user_name=userName).first()
         if user:
             if check_password_hash(user.password, password):
                 flash("Log in successfully", category='success')
-                return redirect(url_for('views.home'))
+                login_user(user, remember=True)
+                if user_type == 'restaurant':
+                    return redirect(url_for('views.home'))
+                return redirect(url_for('views.npo_view'))
 
     return render_template("login.html")
 
@@ -39,10 +44,15 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Your account has been created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('auth.login'))
 
     return render_template("signup.html")
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 class Auth():
     """Test purpose."""
@@ -50,3 +60,4 @@ class Auth():
     def login(self):
         """Test method."""
         print("test")
+
--- website/views.py
+++ website/views.py
@@ -1,16 +1,17 @@
 from flask import Blueprint, render_template, request, flash
 from . import db
-
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
     """Route to home page."""
     return render_template("about.html")
 
-@views.route('/about', methods=['GET'])
-def about():
-    return render_template("about.html")
-
+@views.route('/npo', methods=['GET', 'POST'])
+@login_required
+def npo_view():
+    return "<h1>It works for NPO</h1>"

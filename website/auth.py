40380f76ac434b7360e79be5e84c53a84eb0393d
Paul Miller <paul_john.miller0008@stud.hkr.se>
2021-05-03 13:57:28 +0200

    Changed routing structure

--- website/auth.py
+++ website/auth.py
@@ -18,7 +18,6 @@ def signup():
 
 @auth.route('/login')
 def login():
-<<<<<<< HEAD
     """Login get request."""
     return render_template('login.html')
 
@@ -93,51 +92,6 @@ def signup_post():
 
 
 @auth.route('/logout', methods=['POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
-=======
-    """Route to restaurants users, return login template."""
-    if request.method == 'POST':
-        userName = request.form.get('username')
-        password = request.form.get('password')
-        user_type = request.form.get('org_type')
-
-        user = User.query.filter_by(user_name=userName).first()
-        if user:
-            if check_password_hash(user.password, password):
-                flash("Log in successfully", category='success')
-                login_user(user, remember=True)
-                if user_type == 'restaurant':
-                    return redirect(url_for('views.home'))
-                return redirect(url_for('views.npo_view'))
-
-    return render_template("login.html")
-
-@auth.route('/sign-up', methods=['GET', 'POST'])
-def sign_up():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = User.query.filter_by(user_name=username).first()
-        if user:
-            flash('A restaurant user with same name already exists.', category='error')
-        elif password1 != password2:
-            flash('Password must be the same.', category='error')
-        else:
-            new_user = User(user_name=username, password=generate_password_hash(password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Your account has been created!', category='success')
-            return redirect(url_for('auth.login'))
-
-    return render_template("signup.html")
->>>>>>> main
-
-@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
--- website/models.py
+++ website/models.py
@@ -2,26 +2,15 @@ from . import db
 from flask_login import UserMixin
 
 
-<<<<<<< HEAD
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
     user_type = db.Column(db.String(30), nullable=False)
-=======
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
->>>>>>> main
     foods = db.relationship('Food')
 
 
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     food_name = db.Column(db.String(25))
-<<<<<<< HEAD
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-=======
-    users_id = db.Column(db.Integer, db.ForeignKey('user.id'))
->>>>>>> main

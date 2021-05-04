f1b2705c553b0685913eb6714acc2a537e4e942b
Osagie Osahenagharu <osahenagharu.osagie0005@stud.hkr.se>
2021-05-04 15:56:34 +0200

    unittest for user authentication

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,5 +1,6 @@
+import unittest
 import unittest.mock
-from website import auth
+from website import auth, create_app
 
 
 class TestAuth(unittest.TestCase):
@@ -8,3 +9,42 @@ class TestAuth(unittest.TestCase):
     def test_login(self, mock_print):
         auth.Auth.login(auth)
         mock_print.assert_called_with("test")
+
+    app = create_app()
+
+    def test_auth_create_user(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/signup')
+        self.assertEqual(response.status_code, 200)
+
+        response = tester.post('/signup', data={'username': 'a'})
+        self.assertEqual(response.status_code, 500)
+        response = tester.post(
+            '/signup', data={'username': "osagieosahenagharu@1234", 'password': "osagieosahenagharu@1234"})
+        self.assertEqual(response.status_code, 302)
+        response = tester.post('/signup',
+                               data={'username': "osagieosahenagharu", 'password': "osagieosahenagharu@1234", 'confirm': "osagieosahenagharu@1234"})
+        self.assertEqual(response.status_code, 302)
+
+        response = tester.post('/signup',
+                               data={'username': "osagieosahenagharu",
+                                     'password': "osagieosahenagharu@1234",
+                                     'confirm': "osagieosahenagharu@1234",
+                                     "org_type": "testing"})
+        self.assertEqual(response.status_code, 302)
+
+    def test_auth_login(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/login')
+        self.assertEqual(response.status_code, 200)
+        response = tester.post('/login', data={'username': 'a'})
+        self.assertEqual(response.status_code, 302)
+        response = tester.post('/login', data={'username': "a", 'password': "a"})
+        self.assertEqual(response.status_code, 302)
+        response = tester.post('/login', data={'username': "osagieosahenagharu", 'password': "osagieosahenagharu@1234"})
+        self.assertEqual(response.status_code, 302)
+
+    def test_auth_logout(self):
+        tester = self.app.test_client(self)
+        response = tester.get('/logout')
+        self.assertEqual(response.status_code, 302)

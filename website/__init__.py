b1236163c268d7226a4ec7eb2dd99eb2ed0be468
fizzazeeshan17 <fizzazeeshan123@gmail.com>
2021-05-07 09:24:09 +0200

    food.db

--- website/__init__.py
+++ website/__init__.py
@@ -5,11 +5,13 @@ from os import path
 
 db = SQLAlchemy()
 DB_NAME = 'test.db'
+DB_NAME2 = 'food.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'whhhhhaaatteeverr'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME2}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
@@ -19,10 +21,12 @@ def create_app():
     login_manager.init_app(app)
 
     from .models import User
+    from .models import Food
 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
+
 
     from website.auth import auth
     from website.views import views
@@ -31,6 +35,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
 
     from .models import User
+    from .models import Food
 
     create_table(app)
 
@@ -38,6 +43,11 @@ def create_app():
 
 def create_table(app):
     if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+        
+def create_table(app):
+    if not path.exists('website/' + DB_NAME2):
         db.create_all(app=app)
         print('Created Database!')
 
--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, request, flash
+from flask_login import UserMixin
+from models import Food
 from . import db
 from flask_login import login_required, current_user
 
@@ -14,8 +16,12 @@ def home():
 @views.route('/<user>')
 @login_required
 def dashboard(user):
+    food = Food(title=request.form.get("food"))
+    db.session.add(food)
+    db.session.commit()
+    foods = Food.query.all()
     # Show restaurant page
     if current_user.user_type == 'restaurant':
-        return render_template('restaurant.html', businessname=current_user.businessname)
+        return render_template('restaurant.html', businessname=current_user.businessname,foods=foods)
     # Show NPO page
-    return render_template('npo.html', businessname=current_user.businessname)
+    return render_template('npo.html', businessname=current_user.businessname,foods=foods)

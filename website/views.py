25ca32ad8eac0db337b323a6ee399ea671a060d6
MuazKhan99 <muazkhan99@gmail.com>
2021-05-09 01:14:13 +0200

    fixing

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import UserMixin
+from flask_login.utils import login_user
 from .models import Food
 from . import db
 from flask_login import login_required, current_user
@@ -18,11 +19,22 @@ def home():
 def dashboard(user):
     # Show restaurant page
     if current_user.user_type == 'restaurant' and request.form:
-        food = Food(food_name=request.form.get("food_name"),description=request.form.get("description"),quantity=request.form.get("quantity"))
+        food = Food(id=request.form.get("id"),food_name=request.form.get("food_name"),description=request.form.get("description"),quantity=request.form.get("quantity"))
         db.session.add(food)
         db.session.commit()
     food=Food.query.all()
+    return render_template('restaurant.html', businessname=current_user.businessname, id=current_user.id, food_name=current_user, description=current_user, quantity=current_user, food=food)
+    
+
+@views.route("/update/<int:id>", methods=["POST"])
+@login_required
+def update(id):
+    id = request.form['id']
+    food = Food.query.get_or_404(id)
+    db.session.commit()
+    food=Food.query.all()
     return render_template('restaurant.html', businessname=current_user.businessname, food=food)
+
 
 @views.route("/delete", methods=["POST"])
 @login_required
@@ -31,4 +43,4 @@ def delete():
     food=Food.query.filter_by(id=id).delete()
     db.session.commit()
     food=Food.query.all()
-    return dashboard(current_user)
+    return redirect(url_for("views.dashboard", user=current_user))

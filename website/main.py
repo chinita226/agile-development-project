547c23b762c9042de7fefc6736b5b08ca34886ff
Paul Miller <paul_john.miller0008@stud.hkr.se>
2021-05-10 11:44:43 +0200

    fixed linting errors

--- website/main.py
+++ website/main.py
@@ -1,7 +1,7 @@
-from website import create_app, db
-from website.config import DevSettings
+"""Main module to run the app."""
+from website import create_app
 
-app = create_app(DevSettings)
+app = create_app()
 
 
 if __name__ == "__main__":
--- website/models.py
+++ website/models.py
@@ -1,8 +1,11 @@
+"""Models for objects to be stored in database."""
 from . import db
 from flask_login import UserMixin
- 
- 
+
+
 class User(UserMixin, db.Model):
+    """User object class for database."""
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
@@ -11,11 +14,10 @@ class User(UserMixin, db.Model):
     user_type = db.Column(db.String(30), nullable=False)
     foods = db.relationship('Food')
 
- 
- 
+
 class Food(db.Model):
+    """Food object class for database."""
+
     id = db.Column(db.Integer, primary_key=True)
     food_name = db.Column(db.String(25))
     users_id = db.Column(db.Integer, db.ForeignKey('user.id'))
- 
-

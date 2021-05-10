a2421f6eda541a9c4cf2a4b568f08d6374fce30e
Paul Miller <paul_john.miller0008@stud.hkr.se>
2021-05-10 13:04:02 +0200

    fixed app init file

--- website/__init__.py
+++ website/__init__.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager,  login_manager
 from os import path
 
 db = SQLAlchemy()
-DB_NAME = 'test.db'
 
 def create_app():
     app = Flask(__name__)
@@ -18,7 +17,6 @@ def create_app():
     login_manager.init_app(app)
 
     from .models import User
-    from .models import Food
 
     @login_manager.user_loader
     def load_user(id):
@@ -39,3 +37,27 @@ def create_table(app):
     if not path.exists('website/' + DevSettings.SQLALCHEMY_DATABASE_URI):
         db.create_all(app=app)
         print('Created Database!')
+
+def create_test_app():
+    app = Flask(__name__)
+    app.config.from_object(TestSettings)
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    from website.auth import auth
+    from website.views import views
+
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+
+    return app

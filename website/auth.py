a7b42320c049e06491a367698756ec6b78414c51
MuazKhan99 <muazkhan99@gmail.com>
2021-05-11 00:32:39 +0200

    Co-authored-by: Fizza Zeeshan <fizzazeeshan17@users.noreply.github.com>

--- website/auth.py
+++ website/auth.py
@@ -101,11 +101,10 @@ def signup_post():
     # to display messages to user.
     return redirect(url_for('auth.signup'))
 
-@auth.route('/logout')
+@auth.route('/logout', methods=['POST'])
 @login_required
 def logout():
     logout_user()
-    msg = 'Log out successful!'
-    flash(msg, category='success')
     return redirect(url_for('auth.login'))
+
 

a66ab2c912d819a60f78f84d486e815c849a1edd
Osagie Osahenagharu <osahenagharu.osagie0005@stud.hkr.se>
2021-05-14 01:01:37 +0200

    fine tunning the page and adding adding side nav bar

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from .models import Food, User
-from . import db
 from flask_login import login_required, current_user
+
+from . import db
+from .models import Food
 
 views = Blueprint('views', __name__)
 
@@ -11,9 +12,17 @@ def home():
     """Route to home page."""
     return render_template("about.html")
 
+
+@views.route('/insight')
+def insight():
+    """Route to insight page."""
+    return render_template("insight.html")
+
+
 @views.route('/food-waste')
 def blog():
     return render_template("blog.html")
+
 
 @views.route('/<user>')
 @login_required
@@ -22,14 +31,10 @@ def dashboard(user):
     if current_user.user_type == 'restaurant':
         food = Food.query.filter_by(users_id=current_user.id).all()
         return render_template('restaurant.html', businessname=current_user.businessname, food=food)
-
+    
     food = Food.query.all()
-    users = User.query.all()
     # Show NPO page
-    return render_template('npo.html',
-                           businessname=current_user.businessname,
-                           food=food,
-                           users=users)
+    return render_template('npo.html', businessname=current_user.businessname, food=food)
 
 
 # current_user is the object for the logged in user.
@@ -43,33 +48,30 @@ def add(user):
             description=request.form.get("description"),
             quantity=request.form.get("quantity"),
             users_id=current_user.id
-            )
-
+        )
+        
         db.session.add(food)
         db.session.commit()
         flash("Item added!")
-
+    
     food = Food.query.filter_by(users_id=current_user.id).all()
     return render_template('restaurant.html', businessname=current_user.businessname, food=food)
 
 
-@views.route("/update/<id>", methods=["POST"])
+# Changed the code
+@views.route("/update", methods=["POST"])
 @login_required
-def update(id):
-    id = request.form.get('id')
-    name = request.form.get('name')
-    description = request.form.get('description')
-    quantity = request.form.get('quantity')
-
-    food = Food.query.filter_by(id=id).first()
-
+def update():
+    # Show and Update items in restaurant page
+    _id = request.form.get("id")
+    name = request.form.get("name")
+    des = request.form.get("des")
+    quantity = request.form.get("quantity")
+    food = Food.query.filter_by(id=_id).first()
     food.food_name = name
-    food.description = description
+    food.description = des
     food.quantity = quantity
-
     db.session.commit()
-    flash('Item Updated!')
-
     return redirect(url_for("views.dashboard", user=current_user))
 
 

e112ca452f6b2edcd67b26eae989e4f91a582bc9
Paul Miller <paul_john.miller0008@stud.hkr.se>
2021-05-14 12:25:36 +0200

    first commit

--- website/views.py
+++ website/views.py
@@ -13,17 +13,38 @@ def home():
     """Route to home page."""
     return render_template("about.html")
 
+
 @views.route('/food-waste')
 def blog():
     return render_template("blog.html")
 
-@views.route('/<user>', methods=["POST", "GET"])
+
+@views.route('/<user>')
 @login_required
 def dashboard(user):
     # Show restaurant page
     if current_user.user_type == 'restaurant':
+
         food = Food.query.filter_by(users_id=current_user.id).all()
-        return render_template('restaurant.html', businessname=current_user.businessname, food=food)
+
+        return render_template(
+            'restaurant.html',
+            businessname=current_user.businessname,
+            food=food)
+
+    food = Food.query.order_by(Food.food_name)
+    users = User.query.all()
+
+    # Show NPO page
+    return render_template('npo.html',
+                           businessname=current_user.businessname,
+                           food=food,
+                           users=users)
+
+
+@views.route('/search', methods=["POST"])
+@login_required
+def npo_search():
 
     page = int(request.args.get('page', 1))
     per_page = 5
@@ -31,40 +52,71 @@ def dashboard(user):
     search = False
 
     q = request.args.get('q')
+
     if q:
         search = True
-    if request.method == 'POST' and 'tag' in request.form:
+
+    if 'tag' in request.form:
         tag = request.form["tag"]
-        if not tag:
-            flash("Missing keyword")
-            return redirect(url_for("views.dashboard", user=current_user))
+
         search = "%{}%".format(tag)
-        location = User.query.filter(User.location.like(search)).order_by(User.location)
-        location_for_render=location.limit(per_page).offset(offset)
-        #if location == []:
-        #   flash("not found")
-        #    return render_template('npo.html', businessname=current_user.businessname, food =[], tag=tag)
-        for i in location:
-            food= Food.query.filter_by(users_id=i.id).order_by(Food.food_name)
-        food_for_render= food.limit(per_page).offset(offset)
-        pagination= Pagination(page=page,per_page=per_page,offset=offset, total=food.count(),css_framework='bootstrap3', 
-                           search=search)
-        return render_template('npo.html', businessname=current_user.businessname, food=food_for_render, users=location_for_render, tag=tag,
-        pagination=pagination)
+        location = User.query(User.location.like(search)).order_by(User.location)
+        location_for_render = location.limit(per_page).offset(offset)
+
+        # if location == []:
+        #     flash("not found")
+        #     return render_template('npo.html', businessname=current_user.businessname, food =[], tag=tag)
+        """
+        This if statement below i have commented out so that it works.
+        What if a user searches by restaurant name and not location? You
+        will need to query the database for that too and have another variable
+        that holds the return value for that the same as you have for location.
+        Then you will need to check using if statements if there is a value for
+        location or businessname etc.
+        Also if no results were found for the search term, you will need to handle that
+        also.
+        """
+        # for i in location:
+        #     food = Food.query.filter_by(users_id=i.id).order_by(Food.food_name)
+        #     food_for_render = food.limit(per_page).offset(offset)
+
+        food = Food.query.all()
+        food_for_render = food.limit(per_page).offset(offset)
+
+        pagination = Pagination(
+            page=page,
+            per_page=per_page,
+            offset=offset,
+            total=food.count(),
+            css_framework='bootstrap3',
+            search=search)
+
+        return render_template(
+            'npo.html',
+            businessname=current_user.businessname,
+            food=food_for_render,
+            users=location_for_render,
+            tag=tag,
+            pagination=pagination)
 
     food = Food.query.order_by(Food.food_name)
     users = User.query.order_by(User.id)
     food_for_render = food.limit(per_page).offset(offset)
     users_for_render = users.limit(per_page).offset(offset)
 
-    pagination = Pagination(page=page, per_page=per_page, offset=offset,
-                           total=food.count(), css_framework='bootstrap3', 
-                           search=search)
-    # Show NPO page
+    pagination = Pagination(page=page,
+                            per_page=per_page,
+                            offset=offset,
+                            total=food.count(),
+                            css_framework='bootstrap3',
+                            search=search)
+
     return render_template('npo.html',
                            businessname=current_user.businessname,
                            food=food_for_render,
                            users=users_for_render, pagination=pagination)
+
+
 
 # current_user is the object for the logged in user.
 @views.route('/<user>', methods=["POST"])

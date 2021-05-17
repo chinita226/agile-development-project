ca5d580ece09706bcea7c77ffc35d4db647644b8
cougarhighland <xuan_dung.tran0001@stud.hkr.se>
2021-05-17 18:07:20 +0200

    fix styling and improve filter

--- website/views.py
+++ website/views.py
@@ -1,15 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-<<<<<<< HEAD
-from flask.globals import session
 from .models import Food, User
 from . import db
 from flask_login import login_required, current_user
-from flask_paginate import Pagination
-=======
-from flask_login import login_required, current_user
-from . import db
-from .models import Food, User
->>>>>>> 8d3cb2b1e840e639820fff0158020dcb3a8be5c4
+
 
 views = Blueprint('views', __name__)
 
@@ -39,54 +32,62 @@ def insight():
 def blog():
     return render_template("blog.html")
 
-@views.route('/<user>', methods=["POST", "GET"])
+
+@views.route('/<user>')
 @login_required
 def dashboard(user):
     # Show restaurant page
     if current_user.user_type == 'restaurant':
+
         food = Food.query.filter_by(users_id=current_user.id).all()
-        return render_template('restaurant.html', businessname=current_user.businessname, food=food)
 
-    page = int(request.args.get('page', 1))
-    per_page = 5
-    offset = (page - 1) * per_page
-    search = False
-
-    q = request.args.get('q')
-    if q:
-        search = True
-    if request.method == 'POST' and 'tag' in request.form:
-        tag = request.form["tag"]
-        if not tag:
-            flash("Missing keyword")
-            return redirect(url_for("views.dashboard", user=current_user))
-        search = "%{}%".format(tag)
-        location = User.query.filter(User.location.like(search)).order_by(User.location)
-        location_for_render=location.limit(per_page).offset(offset)
-        #if location == []:
-        #   flash("not found")
-        #    return render_template('npo.html', businessname=current_user.businessname, food =[], tag=tag)
-        for i in location:
-            food= Food.query.filter_by(users_id=i.id).order_by(Food.food_name)
-        food_for_render= food.limit(per_page).offset(offset)
-        pagination= Pagination(page=page,per_page=per_page,offset=offset, total=food.count(),css_framework='bootstrap3', 
-                           search=search)
-        return render_template('npo.html', businessname=current_user.businessname, food=food_for_render, users=location_for_render, tag=tag,
-        pagination=pagination)
+        return render_template(
+            'restaurant.html',
+            businessname=current_user.businessname,
+            food=food)
 
     food = Food.query.order_by(Food.food_name)
-    users = User.query.order_by(User.id)
-    food_for_render = food.limit(per_page).offset(offset)
-    users_for_render = users.limit(per_page).offset(offset)
+    users = User.query.all()
 
-    pagination = Pagination(page=page, per_page=per_page, offset=offset,
-                           total=food.count(), css_framework='bootstrap3', 
-                           search=search)
     # Show NPO page
     return render_template('npo.html',
                            businessname=current_user.businessname,
-                           food=food_for_render,
-                           users=users_for_render, pagination=pagination)
+                           food=food,
+                           users=users)
+
+
+@views.route('/search', methods=["POST"])
+@login_required
+def npo_search():
+
+    tag = request.form["tag"]
+    if not tag:
+        flash("Missing keyword")
+        return redirect(url_for("views.dashboard", user=current_user))
+
+    search = "%{}%".format(tag)
+    location_filter = User.query.filter(User.location.like(search)).one_or_none()
+
+    if location_filter is None:
+        businessname_filter = User.query.filter(User.businessname.like(search)).one_or_none()
+        if businessname_filter is None:
+            flash("Not found")
+            return render_template('npo.html', businessname=current_user.businessname, food=[], users=[], tag=tag)
+
+        businessname = User.query.filter(User.businessname.like(search)).all()
+        for i in businessname:
+            food = Food.query.filter_by(users_id=i.id).all()
+            return render_template('npo.html',
+                                    businessname=current_user.businessname,
+                                    food=food,
+                                    users=businessname,
+                                    tag=tag)
+
+    location = User.query.filter(User.location.like(search)).all()
+    for i in location:
+        food = Food.query.filter_by(users_id=i.id).all()
+        return render_template('npo.html', businessname=current_user.businessname, food=food, users=location, tag=tag)
+
 
 # current_user is the object for the logged in user.
 @views.route('/<user>', methods=["POST"])
@@ -109,20 +110,22 @@ def add(user):
     return render_template('restaurant.html', businessname=current_user.businessname, food=food)
 
 
-
 @views.route("/update/<id>", methods=["POST"])
 @login_required
-def update():
+def update(id):
     # Show and Update items in restaurant page
-    _id = request.form.get("id")
+    id = request.form.get("id")
     name = request.form.get("name")
-    des = request.form.get("des")
+    description = request.form.get("des")
     quantity = request.form.get("quantity")
-    food = Food.query.filter_by(id=_id).first()
+    food = Food.query.filter_by(id=id).first()
     food.food_name = name
-    food.description = des
+    food.description = description
     food.quantity = quantity
+
     db.session.commit()
+    flash('Item Updated!')
+
     return redirect(url_for("views.dashboard", user=current_user))
 
 
